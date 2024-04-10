aab73d3b873dfc263092f352559b498dafadd32e
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2024-04-10 13:56:26 +0300

    printing a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const { argv } = require('process');
 const size = Number(argv[2]);
 const repeat = 'X'.repeat(size);
 if (isNaN(size)) {
-	  console.log('Missing size');
+  console.log('Missing size');
 } else {
-	  for (let i = 0; i < size; i++) {
-		      console.log(repeat);
-		    }
+  for (let i = 0; i < size; i++) {
+    console.log(repeat);
+  }
 }
